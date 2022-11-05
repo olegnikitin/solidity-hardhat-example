f6f15cb618bed39317ac5aa74037a04b75df31da
Oleg Nikitin <olegnikitindev@gmail.com>
2022-11-05 22:33:17 +0200

    playing with contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,7 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  etherscan: {
+    apiKey: "ABCDE12345ABCDE12345ABCDE123456789",
+  },
 };
